50fbe5bb9706ef27c60c5d55306b14ba3a1ac29d
PINGPINGYEE <owb7994@nate.com>
2023-07-26 00:20:50 +0900

    Create roadFindNClosedTraffic.py

---
+++ WOOBIN/roadFindNClosedTraffic.py
@@ -0,0 +1,88 @@
+import folium
+import requests
+from shapely import wkt
+from geopy.distance import great_circle
+import pandas as pd
+
+def get_tmap_route(start_x, start_y, end_x, end_y, app_key):
+    url = "https://apis.openapi.sk.com/tmap/routes/pedestrian"
+    params = {
+        "version": 1,
+        "format": "json",
+        "appKey": app_key,
+        "startX": start_x,
+        "startY": start_y,
+        "endX": end_x,
+        "endY": end_y,
+        "reqCoordType": "WGS84GEO",
+        "resCoordType": "WGS84GEO",
+        "startName": "출발지",
+        "endName": "도착지"
+    }
+    response = requests.post(url, data=params)
+    if response.status_code == 200:
+        data = response.json()
+        if "features" in data:
+            result_data = data["features"]
+            return result_data
+    return None
+
+if __name__ == "__main__":
+    # 데이터 파일 읽어오기
+    data = pd.read_excel('C:\\Users\\오우빈\\Desktop\\POLIO\\오우빈(aiProject)\\DaeguTrafficLight_InFo.xlsx')
+
+    # 'geometry' 컬럼의 데이터를 WKT(Well-Known Text) 형식으로 파싱
+    data['geometry'] = data['geometry'].apply(lambda x: wkt.loads(x))
+
+    # 데이터를 리스트로 변환 (각 원소는 위도와 경도의 튜플)
+    locations = [(geom.y, geom.x) if geom.geom_type == 'Point' else (geom.centroid.y, geom.centroid.x) for geom in data['geometry']]
+
+    # 지도 생성 및 중심 좌표 설정
+    map_center = [sum(lat for lat, lng in locations) / len(locations), sum(lng for lat, lng in locations) / len(locations)]
+    tmap_map = folium.Map(location=map_center, zoom_start=17)
+
+    # 경로 정보를 이용하여 폴리라인 추가
+    start_x = "128.526"
+    start_y = "35.8237"
+    end_x = "128.535990"
+    end_y = "35.836326"
+    
+    app_key = "PpgbRQ84nYWukHJFfAjA3gFoyXrOfLGazEWmhID0"  # 본인의 Tmap API AppKey로 교체해주세요.
+    route_data = get_tmap_route(start_x, start_y, end_x, end_y, app_key)
+
+    if route_data:
+        points = []
+        for feature in route_data:
+            geometry = feature["geometry"]
+            coords = geometry["coordinates"]
+            if geometry["type"] == "Point":
+                points.append((coords[1], coords[0]))
+            elif geometry["type"] == "LineString":
+                for coord in coords:
+                    points.append((coord[1], coord[0]))
+            elif geometry["type"] == "MultiLineString":
+                for line in coords:
+                    for coord in line:
+                        points.append((coord[1], coord[0]))
+
+        if points:
+            # 횡단보도 위치에 마커 추가
+            for lat, lng in locations:
+                for point in points:
+                    if great_circle(point, (lat, lng)).meters < 30:  # 거리 임계값은 적절히 조절
+                        folium.Marker([lat, lng], popup="횡단보도").add_to(tmap_map)
+                        break
+
+            folium.PolyLine(points, color="red", weight=6, opacity=0.7).add_to(tmap_map)
+            print("경로와 근접한 마커를 지도에 표시하였습니다.")
+        else:
+            print("경로 정보를 가져올 수 없습니다.")
+    else:
+        print("경로를 가져올 수 없습니다. 올바른 좌표를 입력했는지 확인하세요.")
+
+    # 지도를 HTML 파일로 저장
+    tmap_map.save("tmap_route_map_with_traffic_light.html")
+
+    # 지도를 보여주기 위해 웹 브라우저로 실행
+    import webbrowser
+    webbrowser.open("tmap_route_map_with_traffic_light.html")
