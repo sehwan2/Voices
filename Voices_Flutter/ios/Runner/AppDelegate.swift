dd6a6e6e1af93b561e42ce6f146c0cb367a72511
KIMMIN5 <alsdh1111@naver.com>
2023-08-19 23:43:44 +0900

    1

--- Voices_Flutter/ios/Runner/AppDelegate.swift
+++ Voices_Flutter/ios/Runner/AppDelegate.swift
@@ -2,6 +2,11 @@ import UIKit
 import Flutter
 import GoogleMaps
 
+func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+  GMSServices.provideAPIKey("AIzaSyB06bzQkcsSutkjH0sZMEC2JX7hm0IGdjA")
+  return true
+}
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
--- Voices_Flutter/lib/main.dart
+++ Voices_Flutter/lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:http/http.dart' as http;
+import 'package:location/location.dart';
 
 void main() {
   runApp(MyApp());
@@ -38,32 +37,40 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   _getCurrentLocation() async {
-    final currentLocation = await fetchCurrentLocationFromTmap();
-    setState(() {
-      _currentLocation = currentLocation;
-    });
-  }
-
-  Future<LocationData?> fetchCurrentLocationFromTmap() async {
-    final url = 'TMAP_API_ENDPOINT';  // Tmap API endpoint를 입력해주세요.
-    final response = await http.get(Uri.parse(url), headers: {
-      'Authorization': 'PpgbRQ84nYWukHJFfAjA3gFoyXrOfLGazEWmhID0',  // 여기에 실제 Tmap API 키를 입력하세요.
-    });
-
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      // API 응답 형식에 따라 적절하게 수정하세요.
-      final latitude = data['latitude'];
-      final longitude = data['longitude'];
-
-      return LocationData.fromMap({
-        'latitude': latitude,
-        'longitude': longitude,
+    final userLocation = await _getUserLocation();
+    if (userLocation != null) {
+      setState(() {
+        _currentLocation = userLocation;
       });
     } else {
-      print('Failed to fetch data from Tmap: ${response.body}');
-      return null;
+      print("Failed to get user's location.");
     }
+  }
+
+  Future<LocationData?> _getUserLocation() async {
+    Location location = new Location();
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return null;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return null;
+      }
+    }
+
+    _locationData = await location.getLocation();
+    return _locationData;
   }
 
   @override
@@ -82,7 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           if (_currentLocation != null)
             Container(
-              height: 300, // 원하는 높이로 조정
+              height: 300,
               child: GoogleMap(
                 onMapCreated: (GoogleMapController controller) {
                   _mapController = controller;
@@ -112,11 +119,10 @@ class Group69 extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Container(
-          width: double.infinity, // 원래의 크기, 필요시 조정
+          width: double.infinity,
           padding: EdgeInsets.all(16.0),
           child: Stack(
             children: [
-              // 검색바 부분
               Container(
                 width: 880,
                 height: 127 / 3,
@@ -145,21 +151,6 @@ class Group69 extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-}
-
-// 가상의 LocationData 클래스
-class LocationData {
-  final double? latitude;
-  final double? longitude;
-
-  LocationData({this.latitude, this.longitude});
-
-  factory LocationData.fromMap(Map<String, dynamic> map) {
-    return LocationData(
-      latitude: map['latitude'],
-      longitude: map['longitude'],
     );
   }
 }
